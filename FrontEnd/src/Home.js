05c363fdc9b09b925a484ea738158159e6c8e758
nnatividad <k4lani@gmail.com>
2024-01-20 18:43:10 -0800

    finished with home page

--- FrontEnd/src/Home.js
+++ FrontEnd/src/Home.js
@@ -1,10 +1,19 @@
 import "./Home.css";
+import NavBar from "./NavBar.js";
+
 function Home(){
     return(
         <div className="BackGroundImage">
+            <img src={process.env.PUBLIC_URL + '/RosehackLogo.png'} alt= "RoseHackLogo" className = "Logo" />
+            <div className="ContentContainer">
+                <>
+                    <NavBar/>
+                </>
             <div className="TitleContainter">
                 <h1>AnimeHub</h1>
+                <p>By Nate Natividad and Vincent Vo</p>
             </div>
+        </div>
       </div>
     );
 }
---
+++ FrontEnd/src/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+
+function NavBar(){
+    return(
+        <nav className = "nav-bar">
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Shows</li>
+          </ul>
+        </div>
+      </nav>
+    );
+}
+
+
+export default NavBar;
